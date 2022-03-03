13862922a6dd7e27d00643b2cfaa72e26a46fe65
Martin Gaida <martingaida.mg@gmail.com>
2022-03-03 16:12:57 -0500

    COMPLETED User Login Route

---
+++ backend/routes/api/session.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+router.post(
+    '/', 
+    asyncHandler(async (req, res, next) => {
+        const { credential, password } = req.body;
+
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials are invalid'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
+    })
+);
+
+module.exports = router;
+
+/*
+fetch('/api/session', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `eDEz5cjI-KSawLB2ohtZwzGl93-HRqXIxmDQ`
+  },
+  body: JSON.stringify({ credential: 'dem@demo.io', password: '1234' })
+}).then(res => res.json()).then(data => console.log(data));
+*/
