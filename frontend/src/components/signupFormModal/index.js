532092a343b407cb71bedfa25d861076d2d7898a
Martin Gaida <Martin@SonyA7sransfers.fios-router.home>
2022-03-10 09:15:46 -0500

    COMPLETED Edit Modal State

--- frontend/src/components/signupFormModal/index.js
+++ frontend/src/components/signupFormModal/index.js
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Modal } from '../../context/modal';
 import SignupForm from '../signupForm';
@@ -8,7 +7,7 @@ const LoginFormModal = () => {
     // const [modalVisible, setModalVisible] = useState(false);
     const dispatch = useDispatch();
     const modalState = useSelector(state => state.modals.modals);
-    
+
     return (
         <>
             <button className='btn-black' onClick={() => setModalVisible(true)}>Get started</button>
