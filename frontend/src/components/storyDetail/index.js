b3ed88d5cc53971353e75e5364b8a7f7d8c9a2a7
Martin Gaida <Martin@SonyA7sransfers.fios-router.home>
2022-03-10 21:15:12 -0500

    COMPLETED Story Detail Functionality

--- frontend/src/components/storyDetail/index.js
+++ frontend/src/components/storyDetail/index.js
@@ -1,54 +1,67 @@
 import { useSelector, useDispatch } from 'react-redux';
 import EditStoryModal from '../editStoryModal';
 import { deleteStory } from '../../store/stories';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import './storyDetail.css';
 
 const StoryDetail = () => {
-    const stories = useSelector(state => state.stories.stories)
-    const session = useSelector(state => state.session.user)
+    const stories = useSelector(state => state.stories.stories);
+    const session = useSelector(state => state.session.user);
     const dispatch = useDispatch();
+    const history = useHistory();
     const { id } = useParams();
     
+    const story = stories?.filter(story => story.id === parseInt(id));
+    // console.log(story);
+    // console.log(id);
+
     return (
         <>
-            <div className='nav-bar-space-background'/>
-            <div className='nav-bar-space-filler'/>
-            <div className='mF-content-main'>
-                <div className='mF-content-story'>
-                    <div className='mF-story-details' onClick={() => console.log('Params id: ', id)}>
-                        {stories?.map(story => {
-                            
-                            return (
-                                <div key={story.id}>
-                                    <div className='mF-story-author'>
-                                        <img className='mF-author-profile' src={require('../../assets/avatars/hal9000.png')}/>
-                                        <p>{story.User.username}</p>
-                                    </div>
-                                    <div className='mF-story-title'>
-                                        <h2>{story.title}</h2>
-                                    </div>
-                                    <div className='mF-story-sample'>
-                                        <p>{story.content}</p>
-                                    </div>
-                                    <div className='mF-story-info'>
-                                        <p>{story.createdAt}</p>
-                                        <p>5 min read</p>
-                                        <p className='mF-story-tag'>Category</p>
-                                    </div>
-                                    {session ? 
-                                        (story.User.id === session.id) && 
-                                            <div className='mF-edit-delete'>
-                                                <EditStoryModal id={story.id}/>
-                                                <button className='btn-plain' onClick={() => dispatch(deleteStory(story.id))}>Delete</button> 
+            {session ?
+                <>
+                    <div className='nav-bar-space-background'/>
+                    <div className='nav-bar-space-filler'/>
+                    <div className='mF-content-main'>
+                        <div className='mF-content-story'>
+                            {stories?.map(story => {
+                                if (story.id === parseInt(id)) {
+                                    return (
+                                        <div className='mF-story-details'>
+                                            <div className='mF-story-author'>
+                                                <img className='mF-author-profile' src={require('../../assets/avatars/hal9000.png')}/>
+                                                <p>{story.User.username}</p>
                                             </div>
-                                        : null}
-                                </div>
-                            )
-                        })}
+                                            <div className='mF-story-title'>
+                                                <h2>{story.title}</h2>
+                                            </div>
+                                            <div className='mF-story'>
+                                                <p>{story.content}</p>
+                                            </div>
+                                            <div className='mF-story-info'>
+                                                <p>{story.createdAt}</p>
+                                                <p>5 min read</p>
+                                                <p className='mF-story-tag'>Category</p>
+                                            </div>
+                                            {(story.User.id === session.id) && 
+                                                <div className='mF-edit-delete'>
+                                                    <EditStoryModal id={story.id}/>
+                                                    <button className='btn-plain' onClick={() => dispatch(deleteStory(story.id))}>Delete</button> 
+                                                </div>
+                                            }
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>
+            : 
+                <>
+                    <div className='nav-bar-space-background'/>
+                    <div className='nav-bar-space-filler'/>
+                    <h1>Only registered users can access this content.</h1>
+                </>
+            }
         </>
     )
 };
